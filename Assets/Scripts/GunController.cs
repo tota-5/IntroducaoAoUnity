8d58b3a2eb7504306e85fcf4dd1cfd47d8fdb29a
Ariel Marcelo Madril <tota5forever@gmail.com>
2021-11-05 18:22:26 -0400

    resolvendo bugs

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -6,6 +6,10 @@ public class GunController : MonoBehaviour
 {
     public GameObject prefabDoTiro;
 
+    public float fireRate = 1;
+
+    float fireTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +19,15 @@ public class GunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        fireTimer += Time.deltaTime;
+
+        if(fireTimer > fireRate)
         {
-            Instantiate(prefabDoTiro, gameObject.transform.position, gameObject.transform.rotation);
+            if (Input.GetMouseButton(0))
+            {
+                fireTimer = 0;
+                Instantiate(prefabDoTiro, gameObject.transform.position, gameObject.transform.rotation);
+            }
         }
     }
 }
